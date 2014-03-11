85328e5bddff145f876c758f21bb988ebdd33ed3
Kumar VVNS <kumar_vvns@yahoo.com>
2014-03-10 21:50:14 -0400

    Third App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThirdApp::Application.config.secret_key_base = 'd083b4b15b32fba8bd4eb2af12965f6e0c53164ff6d6d89f8aa87a2154b9060b63b380d884316848063d918786fe2e10ad250504ee9e32609e031042da7b164e'
